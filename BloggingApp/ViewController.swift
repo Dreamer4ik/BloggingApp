e9c99a584a180d556e4d930b14f22f5eb989b53c
Ivan Potapenko <dreamer4ik@gmail.com>
2021-12-22 12:23:43 +0200

    Initial Commit

---
+++ BloggingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BloggingApp
+//
+//  Created by Ivan Potapenko on 22.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
